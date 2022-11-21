048da85e0a4c5a2ccbca181613e45301f79235e8
Levan D <97022558+Levan-D@users.noreply.github.com>
2022-11-21 23:47:44 +0400

    added card animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import rocketMan from "../assets/rocketdab.png";
+/** @format */
+
+import React from "react"
+import rocketMan from "../assets/rocketdab.png"
 
 const Hero = () => {
   return (
     <section
       id="hero"
-      className="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 sm:pt-16 widescreen:section-min-height tallscreen:section-min-height"
+      className="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 pt-16 widescreen:section-min-height tallscreen:section-min-height"
     >
       <article className="sm:w-1/2">
         <h2 className="max-w-md text-4xl font-bold text-center sm:text-5xl sm:text-left ">
@@ -13,8 +15,8 @@ const Hero = () => {
           Has Gone Before...
         </h2>
         <p className="max-w-md text-xl mt-4 text-center sm:text-left text-blue-300">
-          We're building rockets for the next century today. From the moon to
-          Mars, Jupiter and beyond...
+          We're building rockets for the next century today. From the moon to Mars,
+          Jupiter and beyond...
         </p>
         <p className="max-w-md text-xl mt-4 text-center sm:text-left text-blue-300">
           Think Acme Rockets.
@@ -22,7 +24,7 @@ const Hero = () => {
       </article>
       <img className="w-1/2" src={rocketMan} alt="" />
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import logo from "../assets/logo.png";
+/** @format */
+
+import React, { useState } from "react"
+import logo from "../assets/logo.png"
 
 const Navbar = () => {
-  const [menuVisibility, setMenuVisibility] = useState(false);
+  const [menuVisibility, setMenuVisibility] = useState(false)
   return (
     <nav className=" bg-ACMERed  p-2  sm:scroll-smooth fixed w-full z-50">
       <div className="flex justify-between max-w-4xl m-auto items-center">
@@ -12,21 +14,21 @@ const Navbar = () => {
           </a>
         </div>
         <div className=" items-center hidden sm:flex">
-          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2">
+          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2 border-b-2 border-transparent hover:border-inherit pt-1 ">
             <a href="#rockets">Rockets</a>
           </div>
-          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2">
+          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2 border-b-2 border-transparent hover:border-inherit pt-1">
             <a href="#testimonials">Testimonials</a>
           </div>
-          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2">
+          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2 border-b-2 border-transparent hover:border-inherit pt-1">
             <a href="#about">About</a>
           </div>
-          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2">
+          <div className="mr-6 font-semibold cursor-pointer text-ACMEWhite hover:text-white hover:border-b-2 border-b-2 border-transparent hover:border-inherit pt-1">
             <a href="#contact">Contact</a>
           </div>
         </div>
         <div
-          onClick={() => setMenuVisibility((x) => !x)}
+          onClick={() => setMenuVisibility(x => !x)}
           className="px-3  sm:hidden   block absolute  right-2 text-ACMEWhite cursor-pointer hover:text-white"
         >
           {menuVisibility ? <>&#10005;</> : <>&#9776;</>}
@@ -34,22 +36,34 @@ const Navbar = () => {
       </div>
       {menuVisibility && (
         <div className=" text-center ">
-          <div className="m-5 font-semibold text-ACMEWhite">
+          <div
+            onClick={() => setMenuVisibility(x => !x)}
+            className="m-5 font-semibold text-ACMEWhite"
+          >
             <a href="#rockets">Rockets</a>
           </div>
-          <div className="m-5 font-semibold text-ACMEWhite">
+          <div
+            onClick={() => setMenuVisibility(x => !x)}
+            className="m-5 font-semibold text-ACMEWhite"
+          >
             <a href="#testimonials">Testimonials</a>
           </div>
-          <div className="m-5 font-semibold text-ACMEWhite">
+          <div
+            onClick={() => setMenuVisibility(x => !x)}
+            className="m-5 font-semibold text-ACMEWhite"
+          >
             <a href="#about">About</a>
           </div>
-          <div className="m-5 font-semibold text-ACMEWhite">
+          <div
+            onClick={() => setMenuVisibility(x => !x)}
+            className="m-5 font-semibold text-ACMEWhite"
+          >
             <a href="#contact">Contact</a>
           </div>
         </div>
       )}
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
