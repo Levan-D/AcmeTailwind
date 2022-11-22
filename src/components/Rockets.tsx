870251320f60abca82e0abae60d104b4d7d95920
Levan D <97022558+Levan-D@users.noreply.github.com>
2022-11-23 00:02:08 +0400

    3

--- src/components/Rockets.tsx
+++ src/components/Rockets.tsx
@@ -1,9 +1,9 @@
 /** @format */
 
-import React from "react";
-import rocketMan from "../assets/rocketman.png";
-import rocketLaunch from "../assets/rocketlaunch.png";
-import rocketride from "../assets/rocketride.png";
+import React from "react"
+import rocketMan from "../assets/rocketman.png"
+import rocketLaunch from "../assets/rocketlaunch.png"
+import rocketride from "../assets/rocketride.png"
 
 const Rockets = () => {
   const rocketsData = [
@@ -14,7 +14,7 @@ const Rockets = () => {
     },
     {
       title: "Adventurer",
-      desc: "Best Selling Rocket!",
+      desc: "Best Selling Rocketz!",
       img: rocketride,
     },
     {
@@ -22,7 +22,7 @@ const Rockets = () => {
       desc: "Luxury Starship",
       img: rocketLaunch,
     },
-  ];
+  ]
   return (
     <section
       id="rockets"
@@ -32,19 +32,15 @@ const Rockets = () => {
         Our <span className="text-ACMERed">Rockets</span>
       </h2>
       <ul className="list-none mx-auto my-12 flex text-blue-300 flex-col sm:flex-row items-center gap-8 sm:pt-20">
-        {rocketsData.map((rocket) => (
+        {rocketsData.map(rocket => (
           <li className=" group  flex flex-col items-center rounded-3xl px-2 border-2 border-white w-2/3 sm:w-5/6 py-6 transform transition duration-500 hover:scale-110">
             <img
               className="w-1/2 pt-3 group-hover:animate-rotateY"
               src={rocket.img}
               alt=""
             />
-            <h3 className="text-3xl text-center   font-bold pt-3">
-              {rocket.title}
-            </h3>
-            <p className="text-xl text-center text-ACMEWhite pt-3">
-              {rocket.desc}
-            </p>
+            <h3 className="text-3xl text-center   font-bold pt-3">{rocket.title}</h3>
+            <p className="text-xl text-center text-ACMEWhite pt-3">{rocket.desc}</p>
 
             <a
               className="bg-blue-400 hover:bg-blue-600 active:bg-blue-700 text-white px-3 py-2 font-bold mt-4 rounded-xl border border-solid border-slate-900 dark:border-none w-4/5   text-center"
@@ -56,7 +52,7 @@ const Rockets = () => {
         ))}
       </ul>
     </section>
-  );
-};
+  )
+}
 
-export default Rockets;
+export default Rockets
